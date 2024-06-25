51cd7e3ca0407b8bf26aaeaea19d391497e6b9ea
Cloud Gaming <149394072+CloudGamingServices@users.noreply.github.com>
2024-06-25 18:10:56 +0100

    Add files via upload

---
+++ FiveM Chat/2na_core/fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version 'adamant'
+
+game 'gta5'
+
+description 'www.fivemscript.store'
+
+version '0.2.5'
+
+shared_scripts {
+	'Config.lua',
+	'Common/*.lua'
+}
+
+client_scripts {
+	'Client/*.lua'
+}
+
+server_scripts {
+	'Server/*.lua'
+}
